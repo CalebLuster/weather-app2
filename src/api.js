d23836e3ac834a98b421418de5df006658168e14
Caleb Luster <cwl117@outlook.com>
2022-08-21 14:39:49 -0500

    created search component... turns out you have to subscribe before it will let you use their api

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ff608300a6msh4b558e1f2386314p1bc2b0jsn712d9dfc4e61',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
